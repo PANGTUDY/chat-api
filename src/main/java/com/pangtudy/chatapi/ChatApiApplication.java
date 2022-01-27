dfd726f4630439a06d88bd618903a2e803d524bf
JinhaJjing <tjwlsgkkgslwjt@gmail.com>
2022-01-27 20:16:57 +0900

    CORS 요청 허용 서버 포트 지정

--- src/main/java/com/pangtudy/chatapi/ChatApiApplication.java
+++ src/main/java/com/pangtudy/chatapi/ChatApiApplication.java
@@ -3,11 +3,16 @@ package com.pangtudy.chatapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class ChatApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatApiApplication.class, args);
+        SpringApplication app = new SpringApplication(ChatApiApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "10831"));
+        app.run(args);
     }
 
 }
